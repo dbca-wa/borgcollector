2d811e0e7e7acf952df25c444d926aee7ef0d8fc
Rocky Chen <Rocky.Chen@dpaw.wa.gov.au>
2016-04-20 12:18:08 +0800

    Remove the tail slash from approce and cancel url

--- harvest/urls.py
+++ harvest/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns,  url
 from harvest.views import ApproveJobView, CancelJobView
 
 urlpatterns = patterns('',
-    url(r'^(?P<job_id>\d+)/approve/?$', ApproveJobView.as_view(), name = 'approve_job' ),
-    url(r'^(?P<job_id>\d+)/cancel/?$', CancelJobView.as_view(), name = 'cancel_job' ),
+    url(r'^(?P<job_id>\d+)/approve?$', ApproveJobView.as_view(), name = 'approve_job' ),
+    url(r'^(?P<job_id>\d+)/cancel?$', CancelJobView.as_view(), name = 'cancel_job' ),
 
 )
